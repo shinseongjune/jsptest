094fa9af138f12ee9d153112f29ed316fbedc8f6
shinseongjune <shinseongjune.dev@gmail.com>
2020-10-21 14:12:27 +0900

    자바빈 테스트

--- jsptest/src/jsptest/BeanTest.java
+++ jsptest/src/jsptest/BeanTest.java
@@ -1,5 +1,14 @@
 package jsptest;
 
 public class BeanTest {
+	private String name="홍길동";
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
